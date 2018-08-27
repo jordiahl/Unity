719dc876046af83baa88547695e29345a9249990
jordiahl <jordiahl@ufl.edu>
2018-08-27 10:19:06 -0400

    project 1 and 2 checking if ignore works

---
+++ 2_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        print("hello world");
+        Terminal.WriteLine("this is going to print to the terminal on start up");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
